4e5473e0e3852ce53d1d6aa28f62d343d7e34ec7
muetze85s <56439144+muetze85s@users.noreply.github.com>
2026-07-25 09:06:34 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'truewords-v1.0.1';
+const CACHE_NAME = 'truewords-v1.0.2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -11,7 +11,7 @@ const ASSETS_TO_CACHE = [
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[SW TrueWords v1.0.1] Pre-caching offline assets');
+      console.log('[SW TrueWords v1.0.2] Pre-caching offline assets');
       return cache.addAll(ASSETS_TO_CACHE);
     }).then(() => self.skipWaiting())
   );
